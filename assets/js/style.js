db35903d7e825aea69a62d5ccbf1cf4a48c3cddc
Natalie <kulieshovan4@gmail.com>
2020-04-09 22:17:50 +0300

    Fixed bugs

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,4 +4,13 @@ var page = document.getElementById('stylesheets');
 
 if (hours < 6 || hours > 20) {
     page.setAttribute('href', 'assets/css/dark.css');
+    document.getElementById('slider').checked = true;
+}
+
+function toggleTheme() {
+    if (document.getElementById('slider').checked == true) {
+        page.href = 'assets/css/dark.css';
+    } else {
+        page.href = 'assets/css/main.css';
+    }
 }
