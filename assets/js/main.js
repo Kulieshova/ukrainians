dfa082e2f71f791254142c59d4721a4bb3647f8c
Natalie <kulieshovan4@gmail.com>
2020-04-10 13:52:48 +0300

    wtf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-var currentTime = new Date();
-var hours = currentTime.getHours();
-var page = document.getElementById('stylesheets');
-var slider = document.getElementById('slider');
-
-// Dark theme
-if (hours < 6 || hours > 20) {
-    page.setAttribute('href', 'assets/css/dark.css');
-    slider.checked = true;
-}
-
-// Switch Dark/Light themes
-function toggleTheme() {
-    if (slider.checked == true) {
-        page.href = 'assets/css/dark.css';
-    } else {
-        page.href = 'assets/css/main.css';
-    }
-}
-
 // Main functions
 (function($) {
 
@@ -83,3 +63,23 @@ function toggleTheme() {
 				});
 				
 })(jQuery);
+
+var currentTime = new Date();
+var hours = currentTime.getHours();
+var page = document.getElementById('stylesheets');
+var slider = document.getElementById('slider');
+
+// Dark theme
+if (hours < 6 || hours > 20) {
+    page.setAttribute('href', 'assets/css/dark.css');
+    slider.checked = true;
+}
+
+// Switch Dark/Light themes
+function toggleTheme() {
+    if (slider.checked == true) {
+        page.href = 'assets/css/dark.css';
+    } else {
+        page.href = 'assets/css/main.css';
+    }
+}
