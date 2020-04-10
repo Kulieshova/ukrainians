43b833d6c38bb7af7f32f619e9e7d3f1323e9301
Natalie <kulieshovan4@gmail.com>
2020-04-10 13:20:31 +0300

    Fixed bugs in navbar and switch

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,14 +1,15 @@
 var currentTime = new Date();
 var hours = currentTime.getHours();
 var page = document.getElementById('stylesheets');
+var slider = document.getElementById('slider');
 
 if (hours < 6 || hours > 20) {
     page.setAttribute('href', 'assets/css/dark.css');
-    document.getElementById('slider').checked = true;
+    slider.checked = true;
 }
 
 function toggleTheme() {
-    if (document.getElementById('slider').checked == true) {
+    if (slider.checked == true) {
         page.href = 'assets/css/dark.css';
     } else {
         page.href = 'assets/css/main.css';
